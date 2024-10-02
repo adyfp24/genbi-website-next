7c0c1275c77ff7e43d32185d3999c95d3267d2a4
adyfp24 <adyfp24@gmail.com>
2024-10-02 19:57:06 +0700

    [FEAT] base navbar component

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/layouts/navbar'
+import React from 'react'
+
+const LandingPage: React.FC = () => {
+    return (
+    
+            <div className='bg-sky-100'>
+                <Navbar />
+                LandingPage
+            </div>
+        
+    )
+}
+
+export default LandingPage
--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,9 +1,82 @@
 import React from 'react'
 
-const Navbar : React.FC = () => {
+const Navbar: React.FC = () => {
   return (
     <div>
-        oke ini nav
+      <nav className="bg-transparent ">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+
+          <button data-collapse-toggle="navbar-multi-level" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-multi-level" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+          </button>
+
+          <div className="hidden w-full md:flex md:w-auto" id="navbar-multi-level">
+            <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse mr-10">
+              <img src="/images/GenBI-logo.png" className="w-24 h-auto" alt="GenBI Logo" />
+            </a>
+
+            <ul className="flex flex-col text-dark font-medium p-4 md:p-0 mt-4 border md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
+              <li>
+                <a href="#" className="block py-2 px-2 text-dark md:dark:bg-transparent" aria-current="page">Beranda</a>
+              </li>
+              <li>
+                <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" className="flex items-center justify-between w-full py-2 px-2">Program Kerja <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                </svg></button>
+
+                <div id="dropdownNavbar" className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                  <ul className="py-2 text-sm text-dark" aria-labelledby="dropdownLargeButton">
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                    </li>
+                    <li aria-labelledby="dropdownNavbarLink">
+                      <button id="doubleDropdownButton" data-dropdown-toggle="doubleDropdown" data-dropdown-placement="right-start" type="button" className="flex items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dropdown<svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                      </svg></button>
+                      <div id="doubleDropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="doubleDropdownButton">
+                          <li>
+                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Overview</a>
+                          </li>
+                          <li>
+                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">My downloads</a>
+                          </li>
+                          <li>
+                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Billing</a>
+                          </li>
+                          <li>
+                            <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Rewards</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </li>
+                    <li>
+                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                    </li>
+                  </ul>
+                  <div className="py-1">
+                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                  </div>
+                </div>
+              </li>
+
+              <li>
+                <a href="#" className="block py-2 px-2 ">Artikel</a>
+              </li>
+            </ul>
+
+          </div>
+
+          <div className=''>
+              <button className='rounded-xl px-6 py-2 mr-2 hover:bg-blue-500'>Log in</button>
+              <button className='rounded-xl text-white bg-blue-500 px-6 py-2'>Sign Up</button>
+          </div>
+
+        </div>
+      </nav>
     </div>
   )
 }
