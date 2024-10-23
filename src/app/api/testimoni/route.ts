047c8715ff5803b78362b53595009b6ad1148ad8
adyfp24 <adyfp24@gmail.com>
2024-10-23 15:50:22 +0700

    [FEAT] interface & crud endpoint for testimoni

---
+++ src/app/api/testimoni/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse, NextRequest } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(req: NextRequest) {
+    try {
+        const allTestimoni = await prisma.testimoni.findMany();
+        if(allTestimoni.length === 0) {
+            return NextResponse.json({
+                "success": true,
+                "message": "data testimoni belum tersedia",
+                "data": null
+            }, {status: 200})
+        }
+
+        return NextResponse.json({
+            "success": true,
+            "message": "data testimoni berhasil didapatkan",
+            "data": allTestimoni
+        }, { status: 200 })
+    } catch (error) {
+        return NextResponse.json({
+            "success": false,
+            "message": "internal server error",
+            "error": (error as Error).message
+        }, { status: 500 })
+    }
+}
+
+export async function POST(req: NextRequest) {
+    try {
+        const data : TestimoniCreateRequest = await req.json();
+        const newTestimoni = await prisma.testimoni.create({
+            data:{
+                ...data
+            }
+        })
+
+        return NextResponse.json({
+            "success": true,
+            "message": "Testimoni baru berhasil dibuat",
+            "data": newTestimoni
+        })
+    } catch (error) {
+        return NextResponse.json({
+            "success": false,
+            "message": "internal server error",
+            "error": (error as Error).message
+        }, {status: 500})
+    }
+}
---
+++ src/types/testimoni.ts
@@ -0,0 +1,15 @@
+interface TestimoniUpdateRequest {
+    rating: number,
+    testimoni: string,
+    name: string,
+    instansi: string,
+    videoUrl: string
+}
+
+interface TestimoniCreateRequest {
+    rating: number,
+    testimoni: string,
+    name: string,
+    instansi: string,
+    videoUrl: string
+}
