af50248547fa2f66a20990556cdd288b8a6d234b
adyfp24 <adyfp24@gmail.com>
2024-11-03 22:11:45 +0700

    [FEAT] testimonial context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import 'animate.css'
 import { ArrowRight, ArrowLeft, ChevronDown, Instagram, Facebook, Twitter, Linkedin, Youtube, ChevronUp } from 'lucide-react';
 import lootieFile from '../../public/lootie/Ease-In-Animation-About-GenBI.json';
 import LootieAnimation from '@/components/fragments/lootie-animation';
+import TestimonialSection from '@/components/fragments/testimonial-section';
 
 const LandingPage: React.FC = () => {
     const [openFaqIndex, setOpenFaqIndex] = useState<number | null>(null);
@@ -114,37 +115,7 @@ const LandingPage: React.FC = () => {
                 </div>
             </section>
 
-            <section id='testimonial-section' className='mb-32'>
-                <div className='max-w-screen-xl w-full h-full flex flex-wrap mx-auto bg-pr800 rounded-3xl text-white'>
-                    <div className='w-3/5 flex flex-col justify-around p-12'>
-                        <div className=''>
-                            <img src="images/icon/5-star.png" alt="" />
-                        </div>
-                        <p className='text-4xl'>“Beasiswa Bank Indonesia ngebantu banget kuliah saya! Lewat GenBI, saya dapet banyak pengalaman seru.”</p>
-                        <div className='flex'>
-                            <img className='w-12 h-auto mr-6' src="images/unej-logo.png" alt="" />
-                            <div className=''>
-                                <h4 className='font-semibold text-xl'>- Rizki Dwi Putra</h4>
-                                <h6>Penerima Beasiswa Bank Indonesia Universitas Jember</h6>
-                            </div>
-                        </div>
-                        <div className='flex justify-between items-center'>
-                            <ul className='flex'>
-                                <li className='w-3 h-3 bg-pr300 mr-2 rounded-full'></li>
-                                <li className='w-3 h-3 bg-white mr-2 rounded-full'></li>
-                                <li className='w-3 h-3 bg-white rounded-full'></li>
-                            </ul>
-                            <div className='flex'>
-                                <img className='mr-4' src="images/icon/arrow-left.png" alt="" />
-                                <img src="images/icon/arrow-right.png" alt="" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className='w-2/5'>
-                        <img className='w-full rounded-r-3xl' src="/images/testi-image.png" alt="" />
-                    </div>
-                </div>
-            </section>
+            <TestimonialSection />
 
             <section id='benefit-section'>
                 <div className='max-w-screen-xl w-full h-full flex flex-col mx-auto mb-28'>
