488bca7d9dbd00eb888b8d97e2b4ff86e74ca3ec
adyfp24 <adyfp24@gmail.com>
2024-11-04 16:48:00 +0700

    [FEAT] delete and update endpoint for instansi

--- src/app/api/instansi-penerima/route.ts
+++ src/app/api/instansi-penerima/route.ts
@@ -1,30 +1,31 @@
 import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(){
+export async function GET() {
     try {
         const allInstansi = await prisma.instansi.findMany();
-        if(allInstansi.length === 0) {
-            NextResponse.json({
-                "success" : true,
-                "message" : "data instansi penerima belum ada",
-            }, {status: 200})
+        if (allInstansi.length === 0) {
+            return NextResponse.json({
+                "success": true,
+                "message": "data instansi penerima belum ada",
+            }, { status: 200 })
         }
 
-        NextResponse.json({
+        return NextResponse.json({
             "success": true,
-            "message": "data instansi penerima berhasil didapatkan"
-        }, {status: 200})
+            "message": "data instansi penerima berhasil didapatkan",
+            "data": allInstansi
+        }, { status: 200 })
     } catch (err) {
-        NextResponse.json({
+        return NextResponse.json({
             "success": false,
             "message": "internal server error",
             "error": (err as Error).message
-        }, {status: 500})
+        }, { status: 500 })
     }
 }
 
-export async function POST(req: NextRequest){
+export async function POST(req: NextRequest) {
     try {
         const data = await req.json();
         const newInstansi = await prisma.instansi.create({
@@ -38,10 +39,10 @@ export async function POST(req: NextRequest){
             "data": newInstansi
         })
     } catch (err) {
-        NextResponse.json({
+        return NextResponse.json({
             "success": false,
             "message": "internal server error",
             "error": (err as Error).message
-        }, {status: 500})
+        }, { status: 500 })
     }
 }
